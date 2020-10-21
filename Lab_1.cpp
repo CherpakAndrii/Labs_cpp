0b04d61c0759391aae36c14a07672a0c41255222
Andrii Cherpak <andriycherpak.education@gmail.com>
2020-10-21 11:33:04 +0300

    Лаб№1, В31, С++

---
+++ Lab_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <math.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	double a, b, c, p, S, R, r, bisA, bisB, bisC;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	p = (a + b + c) / 2;
+	S = sqrt(p * (p - a) * (p - b) * (p - c));
+	r = S / p;
+	R = a * b * c / (4 * S);
+	bisA = sqrt(b * c * (a + b + c) * (b + c - a)) / (b + c);
+	bisB = sqrt(a * c * (a + b + c) * (a + c - b)) / (a + c);
+	bisC = sqrt(a * b * (a + b + c) * (a + b - c)) / (a + b);
+	cout << "\nbisector1 = " << bisA << endl;
+	cout << "\nbisector2 = " << bisB << endl;
+	cout << "\nbisector3 = " << bisC << endl;
+	cout << "\nradius of the inscribed circle = " << r << endl;
+	cout << "\nradius of the circumscribed circle = " << R << endl;
+	cout << endl;
+	cout << endl;
+	system("pause");
+}
