83117198af5b6be3221baba1972ead36449a7638
Andrii Cherpak <andriycherpak.education@gmail.com>
2020-10-21 11:56:26 +0300

    comments added

--- Lab_1.cpp
+++ Lab_1.cpp
@@ -1,26 +1,26 @@
-﻿#include <math.h>
+﻿#include <math.h>									//підключаємо необхідні бібліобеки
 #include <stdio.h>
 #include <iostream>
 using namespace std;
-int main()
+int main()											//ініціалізуємо функцію maim()
 {
-	double a, b, c, p, S, R, r, bisA, bisB, bisC;
-	cout << "a = "; cin >> a;
+	double a, b, c, p, S, R, r, bisA, bisB, bisC;	//ініціалізуємо змінні 
+	cout << "a = "; cin >> a;						//вводимо змінні
 	cout << "b = "; cin >> b;
 	cout << "c = "; cin >> c;
-	p = (a + b + c) / 2;
-	S = sqrt(p * (p - a) * (p - b) * (p - c));
-	r = S / p;
-	R = a * b * c / (4 * S);
-	bisA = sqrt(b * c * (a + b + c) * (b + c - a)) / (b + c);
+	p = (a + b + c) / 2;							//Обчислюємо півпериметр
+	S = sqrt(p * (p - a) * (p - b) * (p - c));		//Обчислюємо площу
+	r = S / p;										//Обч. радіус вписаного кола
+	R = a * b * c / (4 * S);						//Обч. радіус описаного кола
+	bisA = sqrt(b * c * (a + b + c) * (b + c - a)) / (b + c);	//обчислюємо бісектриси
 	bisB = sqrt(a * c * (a + b + c) * (a + c - b)) / (a + c);
 	bisC = sqrt(a * b * (a + b + c) * (a + b - c)) / (a + b);
-	cout << "\nbisector1 = " << bisA << endl;
+	cout << "\nbisector1 = " << bisA << endl;		//виводимо необхідні дані
 	cout << "\nbisector2 = " << bisB << endl;
 	cout << "\nbisector3 = " << bisC << endl;
 	cout << "\nradius of the inscribed circle = " << r << endl;
 	cout << "\nradius of the circumscribed circle = " << R << endl;
+	cout << endl;									// два порожніх рядки "для краси"
 	cout << endl;
-	cout << endl;
-	system("pause");
+	system("pause");								// системна пауза, щоб користувач встиг зчитати дані
 }
