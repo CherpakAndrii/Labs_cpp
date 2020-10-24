a9c718c00794d7b408a9a76ce8829fbb46318654
CherpakAndrii <71765448+CherpakAndrii@users.noreply.github.com>
2020-10-24 18:35:01 +0300

    Lab_2_C++
    First commit

---
+++ lab2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	double a, b, c;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	if (cin.fail())
+	{
+		cin.clear();
+		system("cls");
+		cout << "\nInvalid input\n"<<endl;
+		exit(0);
+	}
+	if (a == b || b == c || a == c)
+	{
+		cout << "\nThere is some identical numbers";
+		cout << endl;
+		cout << endl;
+	}
+	else
+		cout << "All numbers are different" << endl;
+	system("pause");
+}
