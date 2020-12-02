522eccfe0f76a7fcde5918776bdd2bca44e6c77b
CherpakAndrii <71765448+CherpakAndrii@users.noreply.github.com>
2020-12-02 16:35:03 +0200

    Add files via upload

--- Lab_6.cpp
+++ Lab_6.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
 #include <stdio.h>
 #include <math.h>
 
 using namespace std;
 
-double eps = 0.00001, x1 = 0.818, x2 = 1.403, x3 = 3.141, area;
 
-double get_area_1() {
+double get_area_1(double x_min, double x_max, double eps) {
 	double a, b, c, s1 = 0;
-	for (double x = x1; x <= x2; x += eps) {
+	for (double x = x_min; x <= x_max; x += eps) {
 		a = log(1 + pow(x, 2));
 		b = exp(-1 * pow(x, 2));
 		c = (a - b) * eps;
@@ -17,9 +16,9 @@ double get_area_1() {
 	return s1;
 }
 
-double get_area_2() {
+double get_area_2(double x_min, double x_max, double eps) {
 	double a, b, c, s2 = 0;
-	for (double x = x2; x <= x3; x += eps) {
+	for (double x = x_min; x <= x_max; x += eps) {
 		a = 2 / x * cos(x / 2);
 		b = exp(-1 * pow(x, 2));
 		c = (a - b) * eps;
@@ -31,7 +30,7 @@ double get_area_2() {
 
 int main()
 {
-	double s1 = get_area_1(), s2 = get_area_2();
+	double eps = 0.00001, x1 = 0.818, x2 = 1.403, x3 = 3.141, area, s1 = get_area_1(x1, x2, eps), s2 = get_area_2(x2, x3, eps);
 	area = s1 + s2;
 	cout << "The area = " << area << endl;
 }
