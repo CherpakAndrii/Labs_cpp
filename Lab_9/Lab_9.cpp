655d1db2685a2701fec891892f1ee3f780094c15
CherpakAndrii <71765448+CherpakAndrii@users.noreply.github.com>
2020-12-23 14:25:57 +0200

    Create Lab_9.cpp

---
+++ Lab_9/Lab_9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Header1.h"
+
+using namespace std;
+
+int main() {
+	int n = 0;
+	string str;
+	char c;
+	cout << "Input a string: ";
+	getline(cin, str);
+	cout << "\nInput the character:"; cin >> c;
+	cout << endl;
+	while (str.length() > 0) { str = get_word(str, c, n); }
+	cout << "\nNumber of changes: " << n << endl;
+}
